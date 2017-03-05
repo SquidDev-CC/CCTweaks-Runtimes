d0db6c00bab736f6a914a9385f244243480400ce
SquidDev <bonzoweb@hotmail.co.uk>
2017-03-05 09:18:05 +0000

    Use reflection to access the config's mod id
    This is now done via a method in 1.10.2+

--- src/main/java/org/squiddev/cctweaks/runtimes/RuntimesMod.java
+++ src/main/java/org/squiddev/cctweaks/runtimes/RuntimesMod.java
@@ -2,12 +2,14 @@ package org.squiddev.cctweaks.runtimes;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.NetworkCheckHandler;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.Logger;
 import org.squiddev.cctweaks.api.CCTweaksAPI;
 
 import java.io.File;
@@ -17,7 +19,6 @@ import java.util.Map;
 	modid = RuntimesMod.ID,
 	name = RuntimesMod.NAME,
 	version = "${mod_version}",
-	dependencies = "required-after:ComputerCraft;",
 	acceptedMinecraftVersions = "[1.8.9,]",
 	guiFactory = "org.squiddev.cctweaks.runtimes.GuiConfigFactory"
 )
@@ -25,17 +26,26 @@ public class RuntimesMod {
 	public static final String ID = "cctweaks-runtimes";
 	public static final String NAME = "CCTweaks Runtimes";
 
+	private static Logger logger;
+
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		org.squiddev.cctweaks.runtimes.ConfigForgeLoader.init(new File(event.getModConfigurationDirectory(), RuntimesMod.ID + ".cfg"));
 		Config.configuration = org.squiddev.cctweaks.runtimes.ConfigForgeLoader.getConfiguration();
+
+		logger = event.getModLog();
 
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
-		Runtimes.register(CCTweaksAPI.instance().luaEnvironment());
+		if (Loader.isModLoaded("CCTweaks") || Loader.isModLoaded("cctweaks")) {
+			logger.info("Injecting additional runtimes into ComputerCraft");
+			Runtimes.register(CCTweaksAPI.instance().luaEnvironment());
+		} else {
+			logger.error("Could not detect CCTweaks. Additional runtimes will not be injected.");
+		}
 	}
 
 	@NetworkCheckHandler
@@ -46,7 +56,21 @@ public class RuntimesMod {
 
 	@SubscribeEvent
 	public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
-		if (eventArgs.modID.equals(RuntimesMod.ID)) {
+		String modID = null;
+
+		try {
+			modID = (String) ConfigChangedEvent.OnConfigChangedEvent.class.getField("modID").get(eventArgs);
+		} catch (Exception ignored) {
+		}
+
+		try {
+			modID = (String) ConfigChangedEvent.OnConfigChangedEvent.class.getMethod("getModID").invoke(eventArgs);
+		} catch (Exception ignored) {
+		}
+
+		if (modID == null) {
+			logger.warn("Could not get the mod id from OnConfigChangedEvent, configs may not update correctly.");
+		} else if (modID.equals(ID)) {
 			org.squiddev.cctweaks.runtimes.ConfigForgeLoader.sync();
 		}
 	}
